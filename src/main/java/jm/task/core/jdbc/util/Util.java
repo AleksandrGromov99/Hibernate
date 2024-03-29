34e0c2b4e6ea056ea1150170f3611ca3ed45a757
AleksandrGromov99 <163206082+AleksandrGromov99@users.noreply.github.com>
2024-03-29 23:30:21 +0300

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,70 +1,74 @@
-package jm.task.core.jdbc.util;
+package jm.task.core.jdbc.model;
 
-import java.sql.*;
-import java.util.Properties;
+import javax.persistence.*;
 
-import jm.task.core.jdbc.model.User;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
+@Entity
+@Table(name = "users")
+public class User {
 
-public class Util {
-    private static final String url = "jdbc:mysql://localhost:3306/mydb";
-    private static final String user = "root";
-    private static final String password = "root";
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
-    private static SessionFactory sessionFactory;
+    @Column(name = "name")
+    private String name;
 
-    public static Connection getConnection() {
-        Connection connection = null;
-        try {
-            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-            connection = DriverManager.getConnection(url, user, password);
-            System.out.println("Connection established");
-        } catch (SQLException e) {
-            throw new RuntimeException("Connection not established");
-        }
-        return connection;
+    @Column(name = "lastname")
+    private String lastName;
+
+    @Column(name = "age")
+    private Byte age;
+
+
+    public User(String name, String lastName, Byte age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
     }
 
-    public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration();
-                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-                // Hibernate settings equivalent to hibernate.cfg.xml's properties
-                Properties settings = new Properties();
-                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, url);
-                settings.put(Environment.USER, user);
-                settings.put(Environment.PASS, password);
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
-                settings.put(Environment.SHOW_SQL, "true");
-
-                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-
-                settings.put(Environment.HBM2DDL_AUTO, "");
-
-                configuration.setProperties(settings);
-
-                configuration.addAnnotatedClass(User.class);
-
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties()).build();
-
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return sessionFactory;
+    public User() {
     }
 
-    public static void closeSessionFactory() {
-        if (Util.getSessionFactory() != null) {
-            Util.sessionFactory.close();
-        }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Byte getAge() {
+        return age;
+    }
+
+    public void setAge(Byte age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
